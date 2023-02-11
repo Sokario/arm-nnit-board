028328343275fd3227302a046632b96a90acf05f
l_saintgermain_catie <l.saintgermain@catie.fr>
2023-02-11 18:58:55 +0100

    Added neural network basic class

---
+++ src/nnit/neural_network/include/neural_network.h
@@ -0,0 +1,19 @@
+#ifndef __NEURAL_NETWORK_H__
+#define __NEURAL_NETWORK_H__
+
+namespace sixtron
+{
+
+class NNIT
+{
+public:
+    NNIT();
+    ~NNIT();
+
+private:
+    /* data */
+};
+
+} // namespace sixtron
+
+#endif // __NEURAL_NETWORK_H__
---
+++ src/nnit/neural_network/src/neural_network.cpp
@@ -0,0 +1,6 @@
+#include "neural_network.h"
+
+namespace sixtron
+{
+    
+} // namespace sixtron
